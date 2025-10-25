e6d22d1376546be8ad64a928960916453c20b922
Yordin <yycruzr@unitru.edu.pe>
2025-10-24 23:11:35 -0500

    Movimiento del jugador completo

--- core/src/main/java/com/yordin/rompe_bloques/Core.java
+++ core/src/main/java/com/yordin/rompe_bloques/Core.java
@@ -1,12 +1,35 @@
 package com.yordin.rompe_bloques;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.yordin.rompe_bloques.entidades.Jugador;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Core implements ApplicationListener {
+
+    float anchoPantalla;
+    float alturaPantalla;
+
+    SpriteBatch batch;
+    float delta;
+
+    Texture texturaPlayer;
+    Jugador player;
+
+
     @Override
     public void create() {
-        // Prepare your application here.
+        anchoPantalla = Gdx.graphics.getWidth();
+        alturaPantalla = Gdx.graphics.getHeight();
+
+        batch = new SpriteBatch();
+        delta = Gdx.graphics.getDeltaTime();
+
+        texturaPlayer = new Texture("nave.png");
+        player = new Jugador(texturaPlayer);
     }
 
     @Override
@@ -20,7 +43,28 @@ public class Core implements ApplicationListener {
 
     @Override
     public void render() {
-        // Draw your application here.
+        logic();
+        input();
+        draw();
+    }
+
+    public void draw(){
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+
+        player.dibujar(batch);
+
+
+        batch.end();
+    }
+    public void logic(){
+        delta = Gdx.graphics.getDeltaTime();
+        player.mover(delta);
+
+    }
+    public void input(){
+
     }
 
     @Override
@@ -35,6 +79,7 @@ public class Core implements ApplicationListener {
 
     @Override
     public void dispose() {
-        // Destroy application's resources here.
+        batch.dispose();
+        texturaPlayer.dispose();
     }
 }
---
+++ core/src/main/java/com/yordin/rompe_bloques/entidades/Jugador.java
@@ -0,0 +1,76 @@
+package com.yordin.rompe_bloques.entidades;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Jugador {
+    private Texture textura;
+    private Rectangle hitBox;
+    private float velocidad;
+
+    public Jugador(Texture textura) {
+        this.textura = textura;
+
+        hitBox = new Rectangle();
+
+        hitBox.width = textura.getWidth() /4f;
+        hitBox.height = textura.getHeight() /4f;
+        hitBox.x = (float) Gdx.graphics.getWidth() / 2;
+        hitBox.y = (float) Gdx.graphics.getHeight()  /100;
+
+        velocidad = 1000;
+    }
+
+    public Jugador(Texture textura, Rectangle hitBox, float velocidad) {
+        this.textura = textura;
+        this.hitBox = hitBox;
+        this.velocidad = velocidad;
+    }
+
+    public void mover(float delta){
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) hitBox.x += velocidad*delta;
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) hitBox.x -= velocidad*delta;
+        limites();
+    }
+
+    private void limites(){
+        if(hitBox.x + hitBox.width >= Gdx.graphics.getWidth()){
+            hitBox.x = Gdx.graphics.getWidth() - hitBox.width - 1;
+        }
+        if(hitBox.x <= 0){
+            hitBox.x = 1;
+        }
+    }
+
+    public void dibujar(SpriteBatch batch){
+        batch.draw(textura, hitBox.x, hitBox.y, hitBox.width, hitBox.height);
+    }
+
+    public float getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(float velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public Texture getTextura() {
+        return textura;
+    }
+
+    public void setTextura(Texture textura) {
+        this.textura = textura;
+    }
+
+    public Rectangle getHitBox() {
+        return hitBox;
+    }
+
+    public void setHitBox(Rectangle hitBox) {
+        this.hitBox = hitBox;
+    }
+}
